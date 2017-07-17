cd1c6ca92a91291ebd2ddbbb6d33b469441e94b9
Julian Osborne <subaruforge@gmail.com>
2017-07-17 16:28:25 -0400

    makeshift chat via console
    this will allow you to send messages and pictures and receive messages and replies for your chosen chat all from the console. to send an image from your pc, type "file//C:\path\of\picture.jpg" and of course, replace it with the actual path of your picture.

--- ConsoleChat.java
+++ ConsoleChat.java
@@ -1,1 +1,22 @@
+public static void chat() {
+        for (int i = 0; i < 999; i++) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String msg = br.readLine();
 
+                if (msg.startsWith("file//")) {
+
+                        String file = delete_to(msg, "file//");
+                        sendImage(file, "@diceyfed");
+
+                    } else {
+
+                        sendConsole("@diceyfed", msg);
+                        System.out.println("You: " + msg + "\n--------------------------");
+
+                    }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }

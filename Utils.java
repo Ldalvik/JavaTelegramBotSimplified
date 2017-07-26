6fe41adf2efc1b1506299d2733ac36bce5258842
Julian Osborne <subaruforge@gmail.com>
2017-07-26 16:47:09 -0400

    trimming utils
    Utilities used to perform odd operations

---
+++ Utils.java
@@ -0,0 +1,16 @@
+public class Utils {
+
+    public static String removeTillWord(String input, String word) {
+        return input.substring(input.indexOf(word));
+    }
+
+    public static String replace(String input, String word, String replace) {
+        return input.replace(word, replace);
+    }
+
+    public static String delete_to(String msg, String trim){
+        String trimStart = removeTillWord(msg, trim);
+        String trimEnd = replace(trimStart, trim, "");
+        return trimEnd;
+    }
+}
